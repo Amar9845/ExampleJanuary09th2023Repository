c2b1e58cb0a43af1c20492a5fb2c5ecd00f6e46c
Amar9845 <aamarmk@gmail.com>
2023-04-07 08:35:56 +0530

    Intial Draft

---
+++ DemoProject/src/test/java/com/sgtesting/tests/Users.java
@@ -0,0 +1,22 @@
+package com.sgtesting.tests;
+
+import org.testng.annotations.Test;
+
+public class Users {
+	@Test
+	public void method1()
+	{
+		System.out.println("is is a method one statement");
+	}
+	@Test
+	public void method2()
+	{
+		System.out.println("is is a method 2 statement");
+	}
+	@Test
+	public void method3()
+	{
+		System.out.println("is is a method 3 statement");
+	}
+
+}
